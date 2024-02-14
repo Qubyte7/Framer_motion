722e4313a82b5209edc43ac545387c7db3979141
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2024-02-14 18:47:41 +0200

    ploting a sample graph

--- plot_audio.py
+++ plot_audio.py
@@ -1,1 +1,22 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
+import wave
+
+sound = wave.open("happy.wav","rb")
+n_frames= sound.getnframes()
+frame_rate = sound.getframerate()
+signal_wave =  sound.readframes(-1)
+sound.close()
+time_for_audio = n_frames /frame_rate
+print(time_for_audio)
+
+signal_array= np.frombuffer(signal_wave, dtype=np.int16)
+times = np.linspace(0, time_for_audio, num=n_frames) 
+
+plt.figure(figsize=(15,5))
+plt.plot(times,signal_array)
+plt.title("Graph for my Audio")
+plt.ylabel("Signal wave")
+plt.xlabel("Time (s)")
+plt.xlim(0, time_for_audio)
+plt.show()
